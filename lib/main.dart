2fc3f4a31efc647faac00be032067264ed82ee27
Utsav Shahi <92775677+shahiutsav@users.noreply.github.com>
2023-05-06 20:15:40 +0545

    Complete UI for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+import 'package:advicer/2_application/core/services/theme_service.dart';
+import 'package:advicer/2_application/pages/features/advice/advice_page.dart';
+import 'package:advicer/theme.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) {
+        return ChangeNotifierProvider(
+          create: (context) => ThemeService(),
+          child: const MainApp(),
+        );
+      },
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +25,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return Consumer<ThemeService>(
+      builder: (context, themeService, child) {
+        return MaterialApp(
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          themeMode:
+              themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          home: const AdvicePage(),
+        );
+      },
     );
   }
 }
